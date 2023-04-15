72e07dce97fb4a89dd17e6125fd43066d71ca674
mesafnt <mesafnt.kalu@ucdenver.edu>
2023-04-15 16:47:23 -0600

    update

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -23,15 +23,30 @@ function movieSet(movie) {
     }
 }
 
+function moviesSet(movies) {
+    return {
+        type: actionTypes.SET_MOVIES,
+        selectedMovies: movies
+    }
+}
+
 export function setMovie(movie) {
     return dispatch => {
         dispatch(movieSet(movie));
     }
 }
 
-export function fetchMovie(movieId) {
+export function setMovies(movies) {
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}/movies/${movieId}?reviews=true`, {
+        dispatch(moviesSet(movies));
+    }
+}
+
+export function fetchMovie(movie_title) {
+    const env = runtimeEnv();
+    console.log('fetch',movie_title)
+    return dispatch => {
+        return fetch(`${env.REACT_APP_API_URL}/movies/${movie_title}?reviews=true`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -45,12 +60,14 @@ export function fetchMovie(movieId) {
             }
             return response.json()
         }).then((res) => {
-            dispatch(movieFetched(res));
+            console.log('res', res)
+            dispatch(movieFetched(res.movie[0]));
         }).catch((e) => console.log(e));
     }
 }
 
 export function fetchMovies() {
+    const env = runtimeEnv();
     return dispatch => {
         return fetch(`${env.REACT_APP_API_URL}/movies?reviews=true`, {
             method: 'GET',
@@ -66,7 +83,54 @@ export function fetchMovies() {
             }
             return response.json()
         }).then((res) => {
-            dispatch(moviesFetched(res));
+            dispatch(moviesFetched(res.movie));
+        }).catch((e) => console.log(e));
+    }
+}
+
+export function postReview(review_data) {
+    const env = runtimeEnv();
+    return dispatch => {
+        return fetch(`${env.REACT_APP_API_URL}/reviews`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            },
+            mode: 'cors',
+            body: JSON.stringify(review_data)
+        }).then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json()
+        }).then((res) => {
+            // console.log(res.json())
+            window.location.reload();
+        }).catch((e) => console.log(e));
+    }
+}
+
+export function searchMovie(search_term) {
+    const env = runtimeEnv();
+    return dispatch => {
+        return fetch(`${env.REACT_APP_API_URL}/search/${search_term}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            },
+            mode: 'cors'
+        }).then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json()
+        }).then((res) => {
+            // dispatch(moviesFetched(res.movie));
+            console.log(res.movie);
         }).catch((e) => console.log(e));
     }
 }
--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -4,54 +4,105 @@ import {connect} from 'react-redux';
 import {Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { BsStarFill } from 'react-icons/bs'
 import { Image } from 'react-bootstrap';
+import {submitLogin} from "../actions/authActions";
+import runtimeEnv from "@mars/render-js-runtime-env";
 
 class MovieDetail extends Component {
 
-    componentDidMount() {
-        const {dispatch} = this.props;
-        if (this.props.selectedMovie == null) {
-            dispatch(fetchMovie(this.props.movieId));
+    constructor(props) {
+        super(props);
+        this.updateDetails = this.updateDetails.bind(this)
+        this.submitReview = this.submitReview.bind(this)
+        this.state = {
+            review_details: {
+                title: "",
+                rating: 0,
+                review: ""
+            }
         }
+
+        this.rating = ''
+        this.message = ''
+    }
+
+
+
+    submitReview(){
+        console.log("in submit review")
+        const {dispatch} = this.props;
+        if (this.state.review_details.review === "" || this.state.review_details.rating === 0) {
+            alert("Empty rating or review");
+        } else {
+            this.state.review_details.title = this.props.selectedMovie.title;
+            console.log('review_details', this.state.review_details)
+            dispatch(postReview(this.state.review_details));
+        }
+
+    }
+
+    updateDetails(event) {
+
+        // console.log("in update details")
+        let updateDetails = Object.assign({}, this.state.review_details)
+
+        updateDetails[event.target.id] = event.target.value;
+
+        this.setState({
+            review_details: updateDetails
+        })
+        // console.log('update details', updateDetails)
     }
 
     render() {
-        const DetailInfo = () => {
-            if (!this.props.selectedMovie) {
-                return <div>Loading....</div>
-            }
 
-            return (
-                <Card>
-                    <Card.Header>Movie Detail</Card.Header>
-                    <Card.Body>
-                        <Image className="image" src={this.props.selectedMovie.imageUrl} thumbnail />
-                    </Card.Body>
-                    <ListGroup>
-                        <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
-                        <ListGroupItem>
-                            {this.props.selectedMovie.actors.map((actor, i) =>
-                                <p key={i}>
-                                    <b>{actor.actorName}</b> {actor.characterName}
-                                </p>)}
-                        </ListGroupItem>
-                        <ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.avgRating}</h4></ListGroupItem>
-                    </ListGroup>
-                    <Card.Body>
-                        {this.props.selectedMovie.reviews.map((review, i) =>
-                            <p key={i}>
-                                <b>{review.username}</b>&nbsp; {review.review}
-                                &nbsp;  <BsStarFill /> {review.rating}
-                            </p>
-                        )}
-                    </Card.Body>
-                </Card>
-            )
+        // const DetailInfo = () => {
+        if (!this.props.selectedMovie) {
+            return <div>Loading....</div>
         }
 
         return (
-            <DetailInfo />
+            <Card>
+                <Card.Header>Movie Detail</Card.Header>
+                <Card.Body>
+                    <Image className="image" src={this.props.selectedMovie.imageUrl} thumbnail/>
+                </Card.Body>
+                <ListGroup>
+                    <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
+                    <ListGroupItem>
+                        {this.props.selectedMovie.actors.map((actor, i) =>
+                            <p key={i}>
+                                <b>{actor.actor_name}</b> {actor.character_name}
+                            </p>)}
+                    </ListGroupItem>
+                    <ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.averaged_rating}</h4></ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                    {this.props.selectedMovie.reviews.map((review, i) =>
+                        <p key={i}>
+                            <b>{review.username}</b>&nbsp; {review.review}
+                            &nbsp;  <BsStarFill/> {review.rating}
+                        </p>
+                    )}
+                </Card.Body>
+                <Form>
+                    <Form.Group controlId="rating">
+                        <Form.Label>Rating</Form.Label>
+                        <Form.Control onChange={this.updateDetails} value={this.state.review_details.rating}
+                                      type="number" min="1" max="5"/>
+                    </Form.Group>
+
+                    <Form.Group controlId="review">
+                        <Form.Label>Review</Form.Label>
+                        <Form.Control onChange={this.updateDetails} value={this.state.review_details.review}
+                                      type="text" placeholder="Write a new review"/>
+                    </Form.Group>
+
+                    <Button onClick={this.submitReview}>Submit Review</Button>
+                </Form>
+            </Card>
         )
     }
+
 }
 
 const mapStateToProps = state => {
@@ -61,4 +112,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(MovieDetail);
-
