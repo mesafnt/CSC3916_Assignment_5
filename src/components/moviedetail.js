10deaeb046e804d7646923fc38905cd1fa9723bc
mesafnt <mesafnt.kalu@ucdenver.edu>
2023-04-23 20:21:51 -0600

    update

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import {fetchMovie, postReview, setMovie} from "../actions/movieActions";
+import { fetchMovie } from "../actions/movieActions";
+import { postReview } from "../actions/movieActions";
 import {connect} from 'react-redux';
-import {Card,  Form, ListGroup, ListGroupItem, Button} from 'react-bootstrap';
+import {Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { BsStarFill } from 'react-icons/bs'
 import { Image } from 'react-bootstrap';
 //import {submitLogin} from "../actions/authActions";
-//import runtimeEnv from "@mars/heroku-js-runtime-env";
-//import {submitLogin} from "../actions/authActions";
 import { Form, FormControl, Button } from 'react-bootstrap';
+
+
 
 class MovieDetail extends Component {
 
